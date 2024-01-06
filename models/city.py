66b68ed45a97783187eeab84832ad194b8f58d9e
John Maleek <jmzoran01@gmail.com>
2024-01-06 09:37:07 +0100

    second commit

--- models/city.py
+++ models/city.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, ForeignKey, String
 from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60),  ForeignKey('states.id'), nullable=False,)
+
+    __tablename__ = "cities"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        state_id = ''
+        name = ''
